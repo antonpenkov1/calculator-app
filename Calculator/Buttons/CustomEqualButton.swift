3f84581b5a024e5547d8d20e00777d9393874bde
anton_penkov <anton.penkov1@gmail.com>
2023-03-30 16:21:07 +0200

    custom buttons

---
+++ Calculator/Buttons/CustomEqualButton.swift
@@ -0,0 +1,44 @@
+//
+//  CustomEqualButton.swift
+//  Calculator
+//
+//  Created by Антон Пеньков on 30.03.2023.
+//
+
+import UIKit
+
+class CustomEqualButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButton()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupButton()
+    }
+    
+    func setupButton() {
+        styleButton()
+    }
+    
+    func styleButton() {
+        setShadow()
+        setTitleColor(.white, for: .normal)
+        backgroundColor = UIColor.orange
+        layer.cornerRadius = 25
+        layer.borderWidth = 3.0
+        layer.borderColor = UIColor.orange.cgColor
+    }
+    
+    private func setShadow() {
+        layer.shadowColor   = UIColor.black.cgColor
+        layer.shadowOffset  = CGSize(width: 0.0, height: 6.0)
+        layer.shadowRadius  = 8
+        layer.shadowOpacity = 0.5
+        clipsToBounds       = true
+        layer.masksToBounds = false
+    }
+    
+}
